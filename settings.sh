490b446a4ba9fe2a83284a77fbf91c6f3ac3a635
Birkir Brynjarsson <birkir@birkir.is>
2018-11-02 22:20:45 +0000

    Add Mac settings and Terminal theme

---
+++ settings.sh
@@ -0,0 +1,162 @@
+defaults write com.apple.dock tilesize -int 48
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock static-only -bool true
+defaults write com.apple.Dock showhidden -bool yes
+defaults write com.apple.dock mru-spaces -bool false
+killall Dock
+
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+# Set Default Finder Location to Home Folder
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable Resume system-wide
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.setup/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Maximize on dobule-clicking 
+defaults write NSGlobalDomain AppleActionOnDoubleClick -string "Maximize"
+
+# Dark interface, Mojave
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+
+# Set alert volume to 0
+defaults write NSGlobalDomain com.apple.sound.beep.volume -float 0.0
+# Flash screen on system beep
+defaults write NSGlobalDomain com.apple.sound.beep.flash -int 1
+
+# Desktop Finder view options
+plutil -replace DesktopViewSettings.IconViewSettings.arrangeBy -string grid ~/Library/Preferences/com.apple.finder.plist
+plutil -replace DesktopViewSettings.IconViewSettings.iconSize -string 48 ~/Library/Preferences/com.apple.finder.plist
+plutil -replace DesktopViewSettings.IconViewSettings.gridSpacing -string 54 ~/Library/Preferences/com.apple.finder.plist
+plutil -replace DesktopViewSettings.IconViewSettings.showItemInfo -string 1 ~/Library/Preferences/com.apple.finder.plist
+
+# Finder settings
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowRecentTags -bool false
+# Finder > Preferences > Hide warning before changing an extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# Finder > Preferences > Keep folders on top: In windows when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true;
+defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true;
+
+# Disable dashboard
+defaults write com.apple.dashboard db-enabled-state -int 1;
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# Top left screen corner → Mission Control
+defaults write com.apple.dock wvous-tl-corner -int 10;
+defaults write com.apple.dock wvous-tl-modifier -int 0;
+defaults write com.apple.dock wvous-tr-corner -int 4;
+defaults write com.apple.dock wvous-tr-modifier -int 0;
+defaults write com.apple.dock wvous-bl-corner -int 2;
+defaults write com.apple.dock wvous-bl-modifier -int 0;
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Don't show Siri in the menu bar
+defaults write com.apple.Siri StatusMenuVisible -bool false
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Hide icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Use column view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `Nlsv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# From https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# Use a modified version of the Japanesque theme by default in Terminal.app
+osascript <<EOD
+tell application "Terminal"
+	local allOpenedWindows
+	local initialOpenedWindows
+	local windowID
+	set themeName to "Japanesque"
+
+	(* Store the IDs of all the open terminal windows. *)
+	set initialOpenedWindows to id of every window
+	(* Open the custom theme so that it gets added to the list
+	   of available terminal themes (note: this will open two
+	   additional terminal windows). *)
+	do shell script "open '$HOME/.setup/terminal/" & themeName & ".terminal'"
+
+	(* Wait a little bit to ensure that the custom theme is added. *)
+	delay 1
+	(* Set the custom theme as the default terminal theme. *)
+	set default settings to settings set themeName
+
+	(* Get the IDs of all the currently opened terminal windows. *)
+	set allOpenedWindows to id of every window
+	repeat with windowID in allOpenedWindows
+		(* Close the additional windows that were opened in order
+		   to add the custom theme to the list of terminal themes. *)
+		if initialOpenedWindows does not contain windowID then
+			close (every window whose id is windowID)
+		(* Change the theme for the initial opened terminal windows
+		   to remove the need to close them in order for the custom
+		   theme to be applied. *)
+		else
+			set current settings of tabs of (every window whose id is windowID) to settings set themeName
+		end if
+	end repeat
+end tell
+EOD
