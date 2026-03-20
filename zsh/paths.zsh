1256d54877ed1a9563266961f4967cedb1db5dfe
Birkir Brynjarsson <birkir@birkir.is>
2026-03-20 22:28:13 +0000

    Add go binaries to PATH

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -39,6 +39,10 @@ source $(brew --prefix nvm)/nvm.sh
 export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
 export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
 
+# Go Setup
+export GOPATH="$HOME/go"
+PATH="$PATH:$GOPATH/bin"
+
 # GPG
 export GPG_TTY=$(tty)
 
